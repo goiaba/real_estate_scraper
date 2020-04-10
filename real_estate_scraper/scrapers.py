3b760240363e26d3de94ec5cd11def161b09ea72
Bruno G M Correa <brunogmc@gmail.com>
2020-04-10 17:33:14 -0300

    Adheres to PEP8 conventions

--- real_estate_scraper/scrapers.py
+++ real_estate_scraper/scrapers.py
@@ -11,8 +11,7 @@ logger = logging.getLogger("real_estate_scraper")
 
 
 def method_none(a_name, a_data):
-    logger.info("No method found to handle request to %s agency.",
-                a_name)
+    logger.info("No method found to handle request to %s agency.", a_name)
     return []
 
 
@@ -37,14 +36,15 @@ def method_one_four(a_name, a_data, result_map_func):
 
 def method_one(a_name, a_data):
     base_url = a_data.get("base_url")
-    return method_one_four(a_name, a_data, lambda e: "%s%s" % (base_url, e.get("url")))
+    return method_one_four(a_name, a_data, lambda e: "%s%s" %
+                           (base_url, e.get("url")))
 
 
 def method_four(a_name, a_data):
     base_url = a_data.get("base_url")
     return method_one_four(
-        a_name, a_data, lambda e: "%s/imoveis/%s" % (base_url, e.get("codigo"))
-    )
+        a_name, a_data, lambda e: "%s/imoveis/%s" %
+        (base_url, e.get("codigo")))
 
 
 def method_five(a_name, a_data):
@@ -64,8 +64,7 @@ def method_five(a_name, a_data):
             map(
                 lambda e: "%s/detalhe-imovel/%s" % (base_url, e.get("codigo")),
                 houses.get("lista"),
-            )
-        )
+            ))
         page += 1
         time.sleep(1)
     return list(house_set)
@@ -75,18 +74,18 @@ def method_two(a_name, a_data):
     headers = get_config("headers")
     parser = etree.HTMLParser()
     base_url = a_data.get("base_url")
-    prepend_base_url_in_search = a_data.get("prepend_base_url_in_search", False)
+    prepend_base_url_in_search = a_data.get("prepend_base_url_in_search",
+                                            False)
     result_search_string = tuple(
-        map(str, a_data.get("result_search_string").split(","))
-    )
+        map(str,
+            a_data.get("result_search_string").split(",")))
     charset = a_data.get("charset", "utf-8")
     page = a_data.get("start_page")
     pagination = a_data.get("pagination", True)
     page_param_separator = a_data.get("page_param_separator", "=")
     if prepend_base_url_in_search:
         result_search_string = tuple(
-            map(lambda e: urljoin(base_url, e), result_search_string)
-        )
+            map(lambda e: urljoin(base_url, e), result_search_string))
     house_set = set([])
     while True:
         url = "%s/%s" % (base_url, a_data.get("query_str"))
@@ -101,10 +100,8 @@ def method_two(a_name, a_data):
         for ref in refs:
             link = ref.get("href", "")
             sanitized_link = urljoin(base_url, link)
-            if (
-                link.startswith(result_search_string)
-                and sanitized_link not in house_set
-            ):
+            if (link.startswith(result_search_string)
+                    and sanitized_link not in house_set):
                 links.add(sanitized_link)
         if not links:
             break
